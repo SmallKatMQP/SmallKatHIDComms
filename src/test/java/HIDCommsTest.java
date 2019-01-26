99b4993f3096eeaeac2332b55e81d192e886d999
keionbis <keion96@gmail.com>
2019-01-25 22:18:50 -0500

    Updated Example File

--- src/test/java/HIDCommsTest.java
+++ src/test/java/HIDCommsTest.java
@@ -1,51 +1,64 @@
 import com.HIDInterface.HIDInterface;
+
+import java.util.Scanner;
+
 public class  HIDCommsTest {
+
+    static final Boolean Enabledebug = false;
 
     public static void main(String[] args) throws InterruptedException {
 
         byte[] msg = new byte[65];
         msg[0] = 0x40;
         msg[1] = (byte) 0x77;
-        msg[2] = (byte) 0x0F;
-        msg[3] = (byte) 0xFA;
-        msg[4] = (byte) 0x07;
-        msg[5] = (byte) 0x77;
-        msg[6] = (byte) 0x02;
-        msg[7] = (byte) 0x44;
-        msg[8] = (byte) 0x09;
-        msg[9] = (byte) 0xFE;
 
-        byte[] reply = new byte[64];
-        HIDInterface iface = new HIDInterface(0x3742, 0x5750, 64);
-        for( int x = 0;x< 100; x++){
-        for (byte b : msg) {
-            System.out.printf(" %02x", b);
+
+        byte[] reply;
+        HIDInterface Motherboard = new HIDInterface(0x3742, 0x5750, 64);
+        byte[] motors;
+        int x;
+
+        motors = getMotorPos();
+        for(x = 0; x<8;x++) {
+            msg[x+2] = motors[x];
         }
+        for (int i = 0;i<1000;i++) {
+            if(Enabledebug) {
+                for (byte b : msg) {
+                    System.out.printf(" %02x", b);
+                }
+                System.out.println(Motherboard.getVendorID() + " " + Motherboard.getProductID() + " " + Motherboard.getPacketLength());
 
-        System.out.println(iface.getVendorID() + " " + iface.getProductID() + " " + iface.getPacketLength());
-        reply = iface.sendMessage(msg);
-        for (byte b : reply) {
-            System.out.printf(" %02x", b);
+            }
+            reply = Motherboard.sendMessage(msg);
+            if(Enabledebug) {
+                for (byte b : reply) {
+                    System.out.printf(" %02x", b);
+                }
+                System.out.println();
+            }
+            Thread.sleep(8);
         }
-            System.out.println();
-             Thread.sleep(8);
-        }
-        iface.EndHID();
-
-
-//        int b = 0x6577;
-//        System.out.printf(String.format("0x%08X",(byte) b));
-//        System.out.printf(String.format("0x%08X",(byte) (b>>8)));
-
-
+        Motherboard.EndHID();
 
     }
 
     public static byte[] angleToBytes(int angle){
         byte[] angleBytes = new byte[2];
-        angleBytes[0] = (byte)(angle>>>8);
+        angleBytes[0] = (byte)(angle>>8);
         angleBytes[1] = (byte)(angle);
-
-        return (angleBytes);
+        return angleBytes;
+    }
+    public static byte[] getMotorPos(){
+        byte[] motorPos = new byte[8];
+        byte[] tmp;
+        Scanner sc = new Scanner(System.in);
+        for(int i = 0;i<8;i+=2) {
+            System.out.println("Enter Motor "+i/2+" pos");
+            tmp = angleToBytes(Integer.parseInt(sc.nextLine()));
+            motorPos[i] = tmp[0];
+            motorPos[i+1] = tmp[1];
+        }
+        return motorPos;
     }
 }
